7b9f2eebf187b802c108536412e25193ce65f078
husniddin <alikkhan1680@gmail.com>
2025-03-25 22:14:26 +0500

    ikkinchi two sum code yahshilanish holati yuklandi

--- LeetCode/Easy_leetCode/Two_Sum_1.py
+++ LeetCode/Easy_leetCode/Two_Sum_1.py
@@ -1,12 +1,29 @@
-# 1, Two Summ    Easy
+# # 1, Two Summ    Easy
+#
+
+# def twoSum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums )):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
+#     return []
+#
+#
+# print(twoSum([2, 2, 4,5,  11, 15, 9], 9))
 
 
 def twoSum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums )):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+    hash_map = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hash_map:
+            return [hash_map[complement], i]
+        hash_map[num] = i
+
     return []
 
 
-print(twoSum([2, 2, 4,5,  11, 15, 9], 9))
+print(twoSum([2, 3, 5, 7, 8], 12))
+
+
