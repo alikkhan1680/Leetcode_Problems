7863873f05a1a7cd2f08e1beccbc1f4fb89394ec
husniddin <alikkhan1680@gmail.com>
2025-04-23 22:15:42 +0500

    comentariya

--- LeetCode/Easy_leetCode/Squares_977.py
+++ LeetCode/Easy_leetCode/Squares_977.py
@@ -1,9 +1,9 @@
 def sortedArray(nums):
-    response = []
+    response = [] #javob uchun yangi list ro'yhati
 
-    for num in nums:
-        square = num ** 2
-        response.append(square)
-    return sorted(response)
+    for num in nums:# for ishlataamiz har bi rqiymat ucun
+        square = num ** 2  #har bir qiymatni  kvadratga ko'taramiz
+        response.append(square) # javob listiga qo'shamiz kvadratga ko'tarilgan qiymatlarni
+    return sorted(response)  #va barcha tayor kvadratga ko'tarilgan variantni tartiblab qaytarammiz
 
 print(sortedArray([-3, 1, 3, 5]))# eng sodda usulda yechilgan masala ishlaydi lekin optimal yechuim emas
